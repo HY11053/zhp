6dbacc8ba7b4d6d080d4bed4cfc4264592ee032c
liang <liang569874@163.com>
2017-08-03 17:42:21 +0800

    v_basic

--- reception/js/index.js
+++ reception/js/index.js
@@ -6,3 +6,32 @@ $("#js_phone_app,#js_add_wx").hover(function(){
 	$(this).removeClass("hover");
 });
 });	
+function ch(t){
+if (t==1)
+{
+ document.getElementById("ch1").className="cur"; 
+ document.getElementById("ch2").className=""; 
+ document.getElementById("ch3").className=""; 
+ document.getElementById("a2_3_1").className="lcur"; 
+ document.getElementById("a2_3_2").className="lnone";
+ document.getElementById("a2_3_3").className="lnone";
+}
+if (t==2)
+{
+ document.getElementById("ch1").className=""; 
+ document.getElementById("ch2").className="cur"; 
+ document.getElementById("ch3").className=""; 
+ document.getElementById("a2_3_1").className="lnone"; 
+ document.getElementById("a2_3_2").className="lcur";
+ document.getElementById("a2_3_3").className="lnone";
+}
+if (t==3)
+{
+ document.getElementById("ch1").className=""; 
+ document.getElementById("ch2").className=""; 
+ document.getElementById("ch3").className="cur"; 
+ document.getElementById("a2_3_1").className="lnone"; 
+ document.getElementById("a2_3_2").className="lnone";
+ document.getElementById("a2_3_3").className="lcur";
+}
+}
